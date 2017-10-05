ac5543abc002334475de87ee48ddb4210e22be57
deant <dean.tesoriero@gmail.com>
2017-10-05 21:41:03 +1100

    Adding dummy class locations for stephen

---
+++ src/main/java/com/itmatcher/config/DatabaseConfig.java
@@ -0,0 +1,10 @@
+package com.itmatcher.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by deant on 10/5/17.
+ */
+@Configuration
+public class DatabaseConfig {
+}
---
+++ src/main/java/com/itmatcher/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.itmatcher.repository;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by deant on 10/5/17.
+ */
+@Repository
+public class UserRepository {
+}
