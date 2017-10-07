609bb2488864cec8e9dc083962352aab8d86a451
Stephen <s3240793@student.rmit.edu.au>
2017-10-08 10:49:55 +1100

    Commit Database Config, SQL Create and SQL Insert files

--- src/main/java/com/itmatcher/config/DatabaseConfig.java
+++ src/main/java/com/itmatcher/config/DatabaseConfig.java
@@ -7,4 +7,15 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class DatabaseConfig {
+    @Bean
+    public DataSource dataSource() {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db = builder
+                .setType(EmbeddedDatabaseType.HSQL)
+                .addScript("sql/create-db.sql")
+                .addScript("sql/insert-data.sql")
+                .build();
+        return db;
+    }
+
 }
