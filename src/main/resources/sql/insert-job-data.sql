bddbccb13f95fd1de6c7d36ae21b8d307bd919e6
Stephen <s3240793@student.rmit.edu.au>
2017-10-19 17:56:31 +1100

    added comment line and fourth job to this file

--- src/main/resources/sql/insert-job-data.sql
+++ src/main/resources/sql/insert-job-data.sql
@@ -1,9 +1,11 @@
--- Insert One Job Record
+-- Insert four Job Record
 INSERT INTO tblJobs(jobsID, freelancerID, jobPosterID, jobDesc, jobAccepted, jobAcceptedDate, jobCompleted, jobCompletedDate, deleteStatus)
   VALUES (100, Null, 2, 'Job No 1 Description', 0, Null, 0, Null, 0);
 INSERT INTO tblJobs(jobsID, freelancerID, jobPosterID, jobDesc, jobAccepted, jobAcceptedDate, jobCompleted, jobCompletedDate, deleteStatus)
   VALUES (101, Null, 2, 'Job No 2 Description', 0, Null, 0, Null, 0);
 INSERT INTO tblJobs(jobsID, freelancerID, jobPosterID, jobDesc, jobAccepted, jobAcceptedDate, jobCompleted, jobCompletedDate, deleteStatus)
   VALUES (102, Null, 4, 'Job No 3 Description', 0, Null, 0, Null, 0);
+INSERT INTO tblJobs(jobsID, freelancerID, jobPosterID, jobDesc, jobAccepted, jobAcceptedDate, jobCompleted, jobCompletedDate, deleteStatus)
+  VALUES (103, Null, 4, 'Job No 4 Description', 0, Null, 0, Null, 0);
 
 
