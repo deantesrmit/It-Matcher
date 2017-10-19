c5bd0d5946712eb35fa37e15420de187a59d1866
Stephen <s3240793@student.rmit.edu.au>
2017-10-19 22:36:35 +1100

    updated create-db, insert-education-data, insert-job-data, insert-profile-data, insert-workexperience-data for consistent field names.

--- src/main/resources/sql/insert-education-data.sql
+++ src/main/resources/sql/insert-education-data.sql
@@ -1,12 +1,12 @@
 -- Insert the list of education levels into the education table
-INSERT INTO tblEducation(education_ID, education_name) VALUES (1, 'Senior Secondary');
-INSERT INTO tblEducation(education_ID, education_name) VALUES (2, 'Certificate I');
-INSERT INTO tblEducation(education_ID, education_name) VALUES (3, 'Certificate II');
-INSERT INTO tblEducation(education_ID, education_name) VALUES (4, 'Certificate III');
-INSERT INTO tblEducation(education_ID, education_name) VALUES (5, 'Certificate IV');
-INSERT INTO tblEducation(education_ID, education_name) VALUES (6, 'Diploma');
-INSERT INTO tblEducation(education_ID, education_name) VALUES (7, 'Advanced Diploma, Associate Degree');
-INSERT INTO tblEducation(education_ID, education_name) VALUES (8, 'Bachelor Degree');
-INSERT INTO tblEducation(education_ID, education_name) VALUES (9, 'Bachelor Honors Degree, Graduate Certificate, Graduate Diploma');
-INSERT INTO tblEducation(education_ID, education_name) VALUES (10, 'Masters Degree');
-INSERT INTO tblEducation(education_ID, education_name) VALUES (11, 'Doctoral Degree');
+INSERT INTO tblEducation(id, educationName) VALUES (1, 'Senior Secondary');
+INSERT INTO tblEducation(id, educationName) VALUES (2, 'Certificate I');
+INSERT INTO tblEducation(id, educationName) VALUES (3, 'Certificate II');
+INSERT INTO tblEducation(id, educationMame) VALUES (4, 'Certificate III');
+INSERT INTO tblEducation(id, educationName) VALUES (5, 'Certificate IV');
+INSERT INTO tblEducation(id, educationName) VALUES (6, 'Diploma');
+INSERT INTO tblEducation(id, educationName) VALUES (7, 'Advanced Diploma, Associate Degree');
+INSERT INTO tblEducation(id, educationName) VALUES (8, 'Bachelor Degree');
+INSERT INTO tblEducation(id, educationName) VALUES (9, 'Bachelor Honors Degree, Graduate Certificate, Graduate Diploma');
+INSERT INTO tblEducation(id, educationName) VALUES (10, 'Masters Degree');
+INSERT INTO tblEducation(id, educationName) VALUES (11, 'Doctoral Degree');
--- src/main/resources/sql/insert-workexperience-data.sql
+++ src/main/resources/sql/insert-workexperience-data.sql
@@ -1,8 +1,8 @@
 -- Insert the list of Work Experience levels into the WorkExp table
-INSERT INTO tblWorkExp(workexp_ID, workexp_name) VALUES (1, '0 to 2 Years');
-INSERT INTO tblWorkExp(workexp_ID, workexp_name) VALUES (2, '3 to 5 Years');
-INSERT INTO tblWorkExp(workexp_ID, workexp_name) VALUES (3, '6 to 9 Years');
-INSERT INTO tblWorkExp(workexp_ID, workexp_name) VALUES (4, '10 to 14 Years');
-INSERT INTO tblWorkExp(workexp_ID, workexp_name) VALUES (5, '15 to 19 Years');
-INSERT INTO tblWorkExp(workexp_ID, workexp_name) VALUES (6, '20+ Years');
+INSERT INTO tblWorkExp(id, workexpName) VALUES (1, '0 to 2 Years');
+INSERT INTO tblWorkExp(id, workexpName) VALUES (2, '3 to 5 Years');
+INSERT INTO tblWorkExp(id, workexpName) VALUES (3, '6 to 9 Years');
+INSERT INTO tblWorkExp(id, workexpName) VALUES (4, '10 to 14 Years');
+INSERT INTO tblWorkExp(id, workexpName) VALUES (5, '15 to 19 Years');
+INSERT INTO tblWorkExp(id, workexpName) VALUES (6, '20+ Years');
 
