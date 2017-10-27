570f41e1f872d9129221d757324b04b5a788f3df
Melissa <s3338093@student.rmit.edu.au>
2017-10-27 14:16:40 +1000

    job offer service opdated

--- src/main/java/com/itmatcher/service/JobOfferService.java
+++ src/main/java/com/itmatcher/service/JobOfferService.java
@@ -10,7 +10,6 @@ import com.itmatcher.util.RequestUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import spark.Request;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
@@ -36,12 +35,11 @@ public class JobOfferService {
   private void mapNewJobOffer(Request request, JobOffer jobOffer) {
     String jobID = getQueryParam(request, "jobID");
     String freelancerID = getQueryParam(request, "freelancerID");
-    Date now;
-    Date date = new SimpleDateFormat("yyyy-MM-dd @ HH:mm").format(now);
+    Date now = new Date();
 
     jobOffer.setJobID(Integer.parseInt(jobID.trim()));
     jobOffer.setFreelancerID(Integer.parseInt(freelancerID.trim()));
     jobOffer.setOfferStatus(0);
-    jobOffer.setLastUpdated(date);
+    jobOffer.setLastUpdated(now);
   }
 }
