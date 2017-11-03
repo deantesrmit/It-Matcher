d9fb7a95f0f033056eee49bcd3dc9683d95e7041
Melissa <s3338093@student.rmit.edu.au>
2017-11-03 19:40:28 +1000

    update register page

--- src/main/java/com/itmatcher/repository/LookupRepository.java
+++ src/main/java/com/itmatcher/repository/LookupRepository.java
@@ -1,6 +1,7 @@
 package com.itmatcher.repository;
 
 import com.itmatcher.domain.Education;
+import com.itmatcher.domain.Skill;
 import com.itmatcher.domain.WorkExp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -19,30 +20,33 @@ import java.util.List;
 
 public class LookupRepository {
     private NamedParameterJdbcTemplate template;
-    public static final String LIST_ALL_EDUCATION = "SELECT*FROM tblEducation";
-    public static final String LIST_ALL_WORK_EXP = "SELECT*FROM tblWorkExp";
+    public static final String LIST_ALL_EDUCATION = "SELECT * FROM tblEducation";
+    public static final String LIST_ALL_WORK_EXP = "SELECT * FROM tblWorkExp";
+
     @Autowired
     public LookupRepository(DataSource ds) {
         template = new NamedParameterJdbcTemplate(ds);
     }
 
 
-    public Optional <List<Education>> getAllEducations() {
+    public List<Education> getAllEducations() {
         Map<String, Object> params = new HashMap<>();
-        List<Education> list = template.query(LIST_ALL_EDUCATION, params, educationRowMapper);
-        if (list != null && !list.isEmpty()) {
-            return Optional.of(list);
-        }
 
-        return Optional.empty();
+        List<Education> list = template.query(LIST_ALL_EDUCATION, params, educationRowMapper);
+        return list;
     }
+
 
     private RowMapper<Education> educationRowMapper = (rs, rowNum) -> {
         Education p = new Education();
         p.setId(rs.getInt("id"));
-        p.setValue(rs.getString("value"));
+        p.setValue(rs.getString("educationName"));
         return p;
     };
+
+
+
+
 
     public Optional <List<WorkExp>> getAllWorkExp() {
         Map<String, Object> params = new HashMap<>();
--- src/main/java/com/itmatcher/service/LookupService.java
+++ src/main/java/com/itmatcher/service/LookupService.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class LookupService {
     @Autowired
     LookupRepository lookupRepository;
-    public Optional <List<Education>> getAllEducations() {
+    public List <Education> getAllEducations() {
         return lookupRepository.getAllEducations();
     }
 }
