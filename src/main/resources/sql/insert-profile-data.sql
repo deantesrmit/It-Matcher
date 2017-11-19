8066d9076ce50f53ff088f7ac02b92f70b9bbc42
bedehickey <bede.hickey@gmail.com>
2017-11-19 15:01:51 +1100

    Added in extra data to the insert files

--- src/main/resources/sql/insert-profile-data.sql
+++ src/main/resources/sql/insert-profile-data.sql
@@ -12,16 +12,21 @@ VALUES (3, 'Perth', '44 Test Street', 'Testville', 'WA', '7777', 'I like to writ
 INSERT INTO tblProfile(userID, location, address1, suburb, state, postCode, bio, education, workExperience)
 VALUES (5, 'Perth', '44 Test Street', 'Testville', 'WA', '7777', 'I like to write Java programs', Null, NULL );
 
--- Insert Two Job Poster Records
+-- Insert Job Poster Records
 INSERT INTO tblUser(id, firstName, lastName, username, email, password, salt, accountType)
 VALUES (2, 'Job', 'poster', 'poster', 'poster@test.com', '$2a$10$ymh/2PpcAV2cqOy6aBF9T.DF/9x8tvvHAB/rE8yRlR18I5oDDOUra', '$2a$10$ymh/2PpcAV2cqOy6aBF9T.', 'POSTER');
 INSERT INTO tblUser(id, firstName, lastName, username, email, password, salt, accountType)
 VALUES (4, 'Job', 'poster2', 'poster2', 'poster2@test.com', '$2a$10$ymh/2PpcAV2cqOy6aBF9T.DF/9x8tvvHAB/rE8yRlR18I5oDDOUra', '$2a$10$ymh/2PpcAV2cqOy6aBF9T.', 'POSTER');
+INSERT INTO tblUser(id, firstName, lastName, username, email, password, salt, accountType)
+VALUES (6, 'Job', 'John', 'Smith', 'JohnTester@gmail.com', '$2a$10$ymh/2PpcAV2cqOy6aBF9T.DF/9x8tvvHAB/rE8yRlR18I5oDDOUra', '$2a$10$ymh/2PpcAV2cqOy6aBF9T.', 'POSTER');
+
 -- Insert a Profile for a Freelancer
 INSERT INTO tblProfile(userID, location, address1, suburb, state, postCode, bio, education, workExperience)
 VALUES ( 2, 'Palm Cove', '22 Test Street', 'Testville', 'QLD', '4444', 'I like to write programs', 1, NULL );
 INSERT INTO tblProfile(userID, location, address1, suburb, state, postCode, bio, education, workExperience)
 VALUES (4, 'Palm Cove', '22 Test Street', 'Testville', 'QLD', '4444', 'I like to write programs', 1, NULL );
+INSERT INTO tblProfile(userID, location, address1, suburb, state, postCode, bio, education, workExperience)
+VALUES (6, 'Sydney', '123 Fake Street', 'Sydney', 'NSW', '2000', 'I am a web development specialist.', 1, NULL );
 
 
 -- Insert the list of skills into the skills table
