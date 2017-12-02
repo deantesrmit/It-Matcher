a061ecf36504473d39861a5f5335c6f7c6f03c60
bedeh <bede.hickey@gmail.com>
2017-12-02 12:53:50 +1100

    Language Class javadocs commenting

--- src/main/java/com/itmatcher/domain/Language.java
+++ src/main/java/com/itmatcher/domain/Language.java
@@ -2,13 +2,23 @@
 package com.itmatcher.domain;
 
 /**
- * @author Dean Tesoriero
+ * Language Class - represents a language for use in job requirements
+ *
+ * @author dean, bede, melissa, john, mark and stephen
+ * @version 1.0
+ *
  */
 public class Language extends WeightedCriteria{
+
+  /**Creates a language without weighting
+   * @param value a string containing the name of the language */
   public Language(String value) {
     this.value = value;
   }
 
+  /**Creates a language with the specified value and weight
+   * @param value a string containing the name of the language
+   * @param weight a CriteriaWeight instance that contains the weighting of the language for a job*/
   public Language(String value, CriteriaWeight weight) {
     this.value = value;
     this.weight = weight;
