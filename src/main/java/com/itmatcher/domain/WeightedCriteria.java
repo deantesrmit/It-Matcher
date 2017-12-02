c962ca04e93eb47e5240962154916e27409d9b1a
bedeh <bede.hickey@gmail.com>
2017-12-02 13:25:36 +1100

    WeightedCriteria Class javadocs commenting

--- src/main/java/com/itmatcher/domain/WeightedCriteria.java
+++ src/main/java/com/itmatcher/domain/WeightedCriteria.java
@@ -2,7 +2,11 @@
 package com.itmatcher.domain;
 
 /**
- * @author Dean Tesoriero
+ * WeightedCriteria Class - represents the weighted criteria for a posted job
+ *
+ * @author dean, bede, melissa, john, mark and stephen
+ * @version 1.0
+ *
  */
 public class WeightedCriteria {
   String value;
@@ -11,23 +15,34 @@ public class WeightedCriteria {
   public WeightedCriteria() {
   }
 
+  /**Creates a Weighted Criteria element with the following parameters
+   * @param value a string containing the value of the weighted criteria
+   * @param weight a CriteriaWeight instance*/
   public WeightedCriteria(String value, CriteriaWeight weight) {
     this.value = value;
     this.weight = weight;
   }
 
+  /**Gets the weight
+   * @return a CriteriaWeight instance containing the weight*/
   public CriteriaWeight getWeight() {
     return weight;
   }
 
+  /**Sets the weight of the CriteriaWeight instance
+   * @param weight a CriteriaWeight instance containing the weight */
   public void setWeight(CriteriaWeight weight) {
     this.weight = weight;
   }
 
+  /**Gets the value
+   * @return a string containing the value of the Weighted Criteria*/
   public String getValue() {
     return value;
   }
 
+  /**Sets the value of the WeightedCriteria instance
+   * @return a string containing the value */
   public void setValue(String value) {
     this.value = value;
   }
