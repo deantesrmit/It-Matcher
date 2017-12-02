a1501f8a719e08d4201ed2a5fee99fd0d9a0c3a2
bedeh <bede.hickey@gmail.com>
2017-12-02 12:03:20 +1100

    Education class commenting

--- src/main/java/com/itmatcher/domain/Education.java
+++ src/main/java/com/itmatcher/domain/Education.java
@@ -1,24 +1,32 @@
 package com.itmatcher.domain;
 
 /**
- * Created by deant on 10/24/17.
+ * Education Class - Represents the education level of a freelancer
+ *
+ * @author dean, bede, melissa, john, mark and stephen
+ * @version 1.0
+ *
  */
 public class Education {
     int id;
     String value;
 
+    /**Gets the id of the education level */
     public int getId() {
         return id;
     }
 
+    /**Sets the id of the education level */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**Gets the value of the education level */
     public String getValue() {
         return value;
     }
 
+    /**Sets the value of the education level */
     public void setValue(String value) {
         this.value = value;
     }
